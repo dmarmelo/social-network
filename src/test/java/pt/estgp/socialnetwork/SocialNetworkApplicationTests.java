d2cf811a52c34e5ed86aede47542acfbdc02ae58
Daniel Marmelo <daniel.marmelo@hotmail.com>
2019-11-18 22:25:02 +0000

    Spring Boot boilerplate for application refactoring. Old application moved to folder legacy-webapp.

---
+++ src/test/java/pt/estgp/socialnetwork/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.estgp.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
